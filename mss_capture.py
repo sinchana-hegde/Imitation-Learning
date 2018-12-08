29d606bab8daa4ebc0bd7a73ec1a46e0c3c127dc
Sinchana Hegde <31339701+sinchana-hegde@users.noreply.github.com>
2018-12-08 14:11:33 +0530

    Update mss_capture.py

--- mss_capture.py
+++ mss_capture.py
@@ -13,3 +13,19 @@ def abc():
     #print("in_mss")
 
     return img
+
+def test():
+with mss.mss() as sct:
+
+		monitor = {'top': 146, 'left': 1910, 'width': 400, 'height': 220}
+		last_time = time.time()
+		img = np.array(sct.grab(monitor))
+		img = cv2.resize(img,(100,100))
+		#img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+		#img = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+		#theta = np.linspace(0., 180., max(img.shape), endpoint=False)
+		#sinogram = radon(img, theta=theta, circle=False)
+		#reconstruction_fbp = iradon(sinogram, theta=theta, circle=False)
+		#img = np.array(reconstruction_fbp,dtype=np.int8)
+
+		return img
